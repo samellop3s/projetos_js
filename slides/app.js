4b89b81991f3905f97a2acfdd992939657d97174
Samuel Ferreira Lopes <96552147+samellop3s@users.noreply.github.com>
2024-02-29 10:02:28 -0300

    Add files via upload

--- slides/app.js
+++ slides/app.js
@@ -9,7 +9,7 @@ const Images = [
     { 'id': '6', 'url':'./img/yuyuhakusho.jpg' },
 ];
 
-const container = document.querySelector('#container-itens');
+const containerItems = document.querySelector('#container-itens');
 
 const loadImages = (images, container) =>  {
     images.forEach(image => {
@@ -21,4 +21,20 @@ const loadImages = (images, container) =>  {
     });
 };
 
-//loadImages(Images, container);
+loadImages(Images, containerItems);    
+
+let items = document.querySelectorAll('.item');
+
+const previous  = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+} 
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
